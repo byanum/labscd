89636dcba023f49bdb0ef0fa9f6c5e594a470061
byanum <ianumkhalid3003@gmail.com>
2020-12-21 18:04:52 +0500

    First Verion

---
+++ PointOfSales/src/pointofsales/Client.java
@@ -0,0 +1,18 @@
+package pointofsales;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Anum
+ */
+public class Client {
+    public static void main(String [] args){
+        Login objectLogin = new Login();
+        objectLogin.setVisible(true);
+    }
+            }
