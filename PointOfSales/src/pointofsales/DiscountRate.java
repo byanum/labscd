22f46ec64fc358c5a09e7fb9d82266d39e5ba76b
byanum <ianumkhalid3003@gmail.com>
2021-01-12 02:04:15 +0500

    version 2.1

---
+++ PointOfSales/src/pointofsales/DiscountRate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pointofsales;
+
+/**
+ *
+ * @author Anum
+ */
+public class DiscountRate {
+      public double rate(double price, int per){
+         return price * per;
+    }
+}
